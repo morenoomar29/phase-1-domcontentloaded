bfedb87c9128cf01d549e9cbfcdc84aae66c7204
Omar <morenoomar29@yahoo.com>
2021-11-08 18:44:04 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code goes here
+
+
+
+/*
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+
+*/
+
+//document.getElementById('text').addEventListener("DOMContentLoaded", () => console.log("This is really cool") )
+/*
+const addingElement = document.getElementById('text')
+addingElement.addEventListener('DOMContentLoaded', function(e) {
+    e.textContent = "This is really cool"
+}
+*/
+//document.getElementById('text').addEventListener("DOMContentLoaded", p.textContent="This is really cool!")
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const p = document.getElementById('text')
+    p.textContent = "This is really cool!"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
